7f68378b95a6636f3dd9fbb70195abc7be0c6d6c
eriansyah20 <eriansyah20@gmail.com>
2017-11-15 13:47:30 +0700

    up

--- application/APP/RByDate/view/VRByDate.js
+++ application/APP/RByDate/view/VRByDate.js
@@ -179,7 +179,7 @@ Ext.define('RByDate.view.VRByDate',{
                     }
                 ]
             });
-            
+           /* 
             Ext.apply(Ext.form.field.VTypes, {
                 DateRange: function(val, field) {
                     var date = field.parseDate(val);
@@ -202,8 +202,9 @@ Ext.define('RByDate.view.VRByDate',{
                     /*
                      * Always return true since we're only using this vtype to set the
                      * min/max allowed values (these are tested for after the vtype test)
-                     */
+                     
                     return true;
                 },
                 DateRangeText: 'From date must be before To date'
             });
+            */
